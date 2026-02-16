630752ebb19b0e6583c36dc0df6649468e6772fd
EBELU <er4416be-s@student.lu.se>
2026-02-16 22:47:33 +0100

    Remove root __init__

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from RaysidClient import RaysidClient, RaysidClientAsync
-from RaysidClient import find_device_by_name
-from RaysidClient import logger
-
-__all__ = ["RaysidClient", "RaysidClientAsync", "find_device_by_name", "logger"]
