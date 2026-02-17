a1769fec047e21a083241cde61b9f1a8304f31a6
EBELU <er4416be-s@student.lu.se>
2026-02-17 16:47:14 +0100

    Added async to imports

--- RaysidClient/__init__.py
+++ RaysidClient/__init__.py
@@ -2,4 +2,4 @@ from .client import RaysidClient, RaysidClientAsync
 from .scanner import find_device_by_name
 from .src.logger import logger
 
-__all__ = ["RaysidClient", "find_device_by_name", "logger"]
+__all__ = ["RaysidClient", "RaysidClientAsync", "find_device_by_name", "logger"]
